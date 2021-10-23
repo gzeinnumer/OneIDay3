b85eed3b88d23fa3fd6c765be29bca2d0ac05be2
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-10-24 00:10:26 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/oneiday3/ConstraintHVActivity.java
@@ -0,0 +1,14 @@
+package com.gzeinnumer.oneiday3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintHVActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_h_v);
+    }
+}
